39f2b140c886b8667f88684b221e6b47b2b5b647
Rabi Ul haq <rs2816868@gmail.com>
2024-04-19 22:20:47 +0500

    Word_Counting_Project
    This is Simple World Counting Project

---
+++ index.js
@@ -0,0 +1,9 @@
+#! /usr/bin.env node 
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "sentence",
+        type: "input",
+        message: " Pleae Enter Your Sentence ",
+    }]);
+const words = answer.sentence.trim().split(" ");
+console.log(words.length);
---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin.env node 
+import inquirer from "inquirer"
+
+
+const answer:{
+  sentence: string
+} = await inquirer.prompt([{
+      name: "sentence",
+      type: "input",
+      message :" Pleae Enter Your Sentence ",
+}])
+const words = answer.sentence.trim().split(" ")
+console.log(words.length)
+ 
